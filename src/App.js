e1f4fde4a874c2ba0845c5ccb722f6a728eadd9f
RudyG252 <122180943+RudyG252@users.noreply.github.com>
2024-04-24 18:34:54 -0700

    I have data now :D

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
 import "./App.css";
-import EpisodeList from "./EpisodeList";
+import { Card, Paper, Button } from "@mui/material";
+import { CardHeader, CardContent } from "@mui/material";
 import EpisodeDescription from "./EpisodeDescription";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() =>  {
-    setEpisodes(setData);
-  }, [setData]);
-  return (
-    <div className="App">
-      <h1>Big Bang Theory Episode Directory</h1>
-      <div className="display">
-        <span className="episodeList" data = {data} >
-          <EpisodeList />
-        </span>
-        <span className="episodeDescription" data = {data}>
-          <EpisodeDescription />
-        </span>
-      </div>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [currentEpisodeData, setCurrentEpisodeData] = useState("");
+    useEffect(() => {
+        fetch(
+            "https://us-central1-big-bang-theory-25fd5.cloudfunctions.net/bbt592622/bbt/episodes"
+        ).then((results) => results.json())
+        .then((data) =>{
+            // console.log(data.data._embedded.episodes);
+            setData(data.data._embedded.episodes);
+        })
+      }, [currentEpisodeData]);
+    if (data.length == 0) {
+        return (<>Loading...</>)
+    }
+    console.log(data);
+    console.log(data[0].summary)
+    return (
+        <div className="App">
+            <div className="Header">
+                <h1>Big Bang Theory Episode Directory</h1>
+            </div>
+            <span className="display">
+                <Card className="episodeList">
+                    <CardHeader title="Episode List" />
+                    <CardContent className="episodeListContent">
+                        {createEpisodeList(
+                            data,
+                            setCurrentEpisodeData
+                        )}
+                    </CardContent>
+                </Card>
+                <EpisodeDescription
+                    data={currentEpisodeData}
+                    setData={setCurrentEpisodeData}
+                />
+            </span>
+        </div>
+    );
 }
 
-async function setEpisodes(setData) {
-    const response = await fetch(
-      "https://us-central1-big-bang-theory-25fd5.cloudfunctions.net/bbt592622/bbt/episodes"
+function episodeButton(episodeTitle, episodeData, setCurrentEpisodeData) {
+    return (
+        <Button
+            variant="outlined"
+            onClick={() => {
+                setCurrentEpisodeData(episodeData);
+            }}
+            className="EpisodeTitle"
+        >
+            {episodeTitle}
+        </Button>
     );
-    const episodes = await response.json();
-    console.log(episodes);
-    setData(episodes);
-  }
+}
+
+function createEpisodeList(dataArr, setCurrentData) {
+    return (
+        <ul>
+            {dataArr.map((item) => (
+                <li key={item}>{episodeButton(item.name, item, setCurrentData)}</li>
+            ))}
+        </ul>
+    );
+}
 
 export default App;
--- src/EpisodeDescription.js
+++ src/EpisodeDescription.js
@@ -1,9 +1,22 @@
+import { Card, Paper, Button } from "@mui/material";
+import { CardHeader, CardContent } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import "./EpisodeDescription.css"
+
 
 
 function EpisodeDescription(props) {
-    return (
-        <h1>Episode Description</h1>
-    )
+  let data = props.data;
+  return (
+    <Card>
+      <CardHeader title="Episode Description" />
+      <CardContent className="episodeDescriptionContent">
+
+        {/* {data.name} */}
+        {data.summary}
+      </CardContent>
+    </Card>
+  );
 }
 
 export default EpisodeDescription;
